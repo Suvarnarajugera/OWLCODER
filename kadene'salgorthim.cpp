1306f701949df03b9a5fbefe4ffcbe60b8be78f5
Suvarna raju Gera <143683042+Suvarnarajugera@users.noreply.github.com>
2024-01-18 21:11:33 +0530

    Add files via upload

---
+++ kadene'salgorthim.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >>n;
+    int arr[n];
+    for(int i = 0 ; i < n ; i++){
+        cin >> arr[i];
+    }
+    int curr = 0;
+    int mx = 0;
+    for(int i = 0 ; i < n ; i++){
+        curr += arr[i];
+        if(curr < 0){
+            curr = 0;
+        }
+        mx = max(mx,curr);
+    }
+    cout << mx;
+}
