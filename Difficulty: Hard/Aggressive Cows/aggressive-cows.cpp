c8704a177fd1fef1f8c469d96ef505f7c40d7c8e
Suvarna raju Gera <143683042+Suvarnarajugera@users.noreply.github.com>
2024-08-01 14:02:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Aggressive Cows/aggressive-cows.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+public:
+    bool Func(vector<int>&stalls, int dist, int cows) {
+        int n = stalls.size(); //size of array
+        int cntCows = 1; //no. of cows placed
+        int last = stalls[0]; //position of last placed cow.
+        for (int i = 1; i < n; i++) {
+            if (stalls[i] - last >= dist) {
+                cntCows++; //place next cow.
+                last = stalls[i]; //update the last location.
+            }
+            if (cntCows >= cows) return true;
+        }
+        return false;
+    }
+    int solve(int n, int k, vector<int> &stalls) {
+        sort(stalls.begin(), stalls.end());
+        int low = 1;
+        int high = stalls[n - 1] - stalls[0];
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            if (Func(stalls, mid, k) == true) {
+                low = mid + 1;
+            }
+            else high = mid - 1;
+        }
+        return high;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while (t--) {
+        // Input
+
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> stalls(n);
+        for (int i = 0; i < n; i++) {
+            cin >> stalls[i];
+        }
+        // char ch;
+        // cin >> ch;
+
+        Solution obj;
+        cout << obj.solve(n, k, stalls) << endl;
+
+        // cout << "~\n";
+    }
+    // fclose(stdout);
+
+    return 0;
+}
+// } Driver Code Ends
